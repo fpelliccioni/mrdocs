167f6c3acd7d27a95049de1c24c3c7249c54e186
Fernando Pelliccioni <fpelliccioni@gmail.com>
2024-01-10 10:41:27 +0100

    wip

--- src/lib/Lib/ConfigImpl.hpp
+++ src/lib/Lib/ConfigImpl.hpp
@@ -134,10 +134,6 @@ public:
             files referenced in the documentation.
          */
         std::string baseURL;
-
-        // /** A string holding the complete CMake arguments.
-        // */
-        // std::string cmake;        
     };
 
     /// @copydoc Config::settings()
