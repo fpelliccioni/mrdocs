8077106a3d95677ec2452fc1f941b2a8d7f46660
Fernando Pelliccioni <fpelliccioni@gmail.com>
2024-01-25 17:35:53 +0100

    wip

--- src/lib/Support/Path.cpp
+++ src/lib/Support/Path.cpp
@@ -417,13 +417,13 @@ ScopedTempFile(
 {
     llvm::SmallString<128> tempPath;
     ok_ = !llvm::sys::fs::createTemporaryFile(prefix, ext, tempPath);
-    report::info("ScopedTempFile::ScopedTempFile() - ok_:      {}", ok_);
-    report::info("ScopedTempFile::ScopedTempFile() - tempPath: {}", tempPath.str().str());
+    // report::info("ScopedTempFile::ScopedTempFile() - ok_:      {}", ok_);
+    // report::info("ScopedTempFile::ScopedTempFile() - tempPath: {}", tempPath.str().str());
     if (ok_)
     {
-        path_ = files::makeDirsy(path_.str());
+        path_ = files::makeDirsy(tempPath.str());
     }
-    report::info("ScopedTempFile::ScopedTempFile() - path_: {}", path_.str());
+    // report::info("ScopedTempFile::ScopedTempFile() - path_: {}", path_.str());
 }
 
 ScopedTempDirectory::
