8fd740c86ac6f653e9a5f49e9d8e7180b31f3674
Fernando Pelliccioni <fpelliccioni@gmail.com>
2024-03-29 07:59:40 -0300

    fixes PR comments

--- src/lib/AST/ASTVisitor.cpp
+++ src/lib/AST/ASTVisitor.cpp
@@ -2190,17 +2190,7 @@ public:
             return;
 
         I.Name = extractName(D);
-        I.FullyQualifiedName = buildNameInfo(D);
-
-        // A NamedDecl nominated by a NamespaceAliasDecl
-        // will be one of the following:
-        // - NamespaceDecl
-        if(NamedDecl* ND = D->getAliasedNamespace())
-        {
-            SymbolID id;
-            getDependencyID(ND, id);
-            I.AliasedSymbol = id;
-        }
+        I.AliasedSymbol = buildNameInfo(D);
 
         getParentNamespaces(I, D);
     }
