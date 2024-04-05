ec5299f1fd679675f0168bdb0ef52ac316bc9ff7
Fernando Pelliccioni <fpelliccioni@gmail.com>
2024-04-05 10:59:03 -0300

    Update src/lib/Gen/xml/XMLWriter.cpp
    Co-authored-by: Krystian Stasiowski <sdkrystian@gmail.com>

--- src/lib/Gen/xml/XMLWriter.cpp
+++ src/lib/Gen/xml/XMLWriter.cpp
@@ -443,12 +443,8 @@ XMLWriter::
 
     writeJavadoc(I.javadoc);
 
-    for (auto const& symbol : I.UsingSymbols)
-    {
-        Attributes attrs = {};
-        attrs.push({"id", toString(symbol)});
-        tags_.write("symbol", {}, attrs);
-    }
+    for (auto const& id : I.UsingSymbols)
+        tags_.write("named", {}, { id });
 
     if (I.Qualifier)
     {
