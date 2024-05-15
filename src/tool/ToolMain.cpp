779b70e30caee2bd8327d1cb5f9681c378f496bc
Fernando Pelliccioni <fpelliccioni@gmail.com>
2024-05-15 16:45:18 +0200

    wip

--- src/tool/ToolMain.cpp
+++ src/tool/ToolMain.cpp
@@ -38,11 +38,19 @@ print_version(llvm::raw_ostream& os)
        << "\n    built with LLVM " << LLVM_VERSION_STRING;
 }
 
+void
+customFatalErrorHandler(void *userData, const std::string &reason, bool genCrashDiag)
+{
+    llvm::sys::PrintStackTrace(llvm::errs());
+}
+
+
 int
 mrdocs_main(int argc, char const** argv)
 {
     // Enable stack traces
     llvm::EnablePrettyStackTrace();
+    llvm::sys::SetFatalErrorHandler(customFatalErrorHandler);
     llvm::sys::PrintStackTraceOnErrorSignal(argv[0], true);
 
     // Parse command line options
